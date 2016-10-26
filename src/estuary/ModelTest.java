81338e0feb9acc19c4b415e4cb9c17cc91a12dbb
gppalmer <gpalmer@udel.edu>
2016-10-26 14:47:37 -0400

    Update ModelTest.java

--- src/estuary/ModelTest.java
+++ src/estuary/ModelTest.java
@@ -72,11 +72,55 @@ public class ModelTest {
 		
 		mazeGame.onTick();
 		
-		assertEquals("When MazeCrab with full health eats Kelp, the kelp goes away, health doesn't increase",
-					 true, mazeGame.mazeCrab.getHealth() == 3 &&
-					       mazeGame.kelpPlants.size() == 2);
+		assertEquals("When MazeCrab with full health eats Kelp, health doesn't increase",
+					 true, mazeGame.mazeCrab.getHealth() == 3;
 	}
-	
+			     
+	@Test
+	public void test_004() {
+		
+		ModelMazeGame mazeGame = createTestMazeGame1();
+		
+		// possible initial adjustments
+		mazeGame.mazeCrab.setHealth(MazeCrab.MAX_HEALTH);
+		
+		mazeGame.onTick();
+		
+		assertEquals("When MazeCrab with full health eats Kelp, kelp goes away,
+					 2, mazeGame.kelpPlants.size());
+	}
+			     
+	@Test
+	public void test_005() {
+		
+		ModelMazeGame mazeGame = createTestMazeGame1();
+		
+		// possible initial adjustments
+		mazeGame.predators.add(new Predator(new Vector2(12.0,12.0));
+		mazeCrab.setHealth(mazeCrab.MAX_HEALTH);
+				      
+		mazeGame.onTick();
+		
+		assertEquals("When MazeCrab collides with predator, mazeCrab health decreases
+					 mazeCrab.MAX_HEALTH - 1, mazeGame.mazeCrab.getHealth());
+			
+	}
+				  
+	public void test_006() {
+		
+		ModelMazeGame mazeGame = createTestMazeGame1();
+		
+		// possible initial adjustments
+		mazeGame.predators.add(new Predator(new Vector2(12.0,12.0));
+		mazeCrab.setHealth(1);
+				      
+		mazeGame.onTick();
+		
+		assertEquals("When MazeCrab collides with predator and has one health, the game ends
+					 true, mazeGame.isEnd();
+			
+	}
+			
 	
 	/*****************************
 	 * Counting Game Model Tests *
